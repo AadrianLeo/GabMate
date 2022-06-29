2e041bd48f54fcc43d84828ebcfbc743515f1abb
DHARAMPAL SINGH <ravisingh02082001@gmail.com>
2022-06-30 00:04:11 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 const cookies = new Cookies();
 
-const apiKey = 'nn4mvfsvemfb';
+const apiKey = '<YOUR_STREAMCHAT_API_KEY>';
 const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
